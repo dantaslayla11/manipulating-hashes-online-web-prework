75067c3094a04bf063f4e100dc7bc6a2450b3856
layla dantas <laylatdantas@gmail.com>
2019-05-24 04:32:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
      v.each do |k1,v1|
        if k1 ==:favorite_icecream_flavors
         v1.delete_if?("strawberry")
-         end 
+        
          
        end
      end    
