6db801a7b0145a42bb13d0e2ea4ca25f2fdc02be
layla dantas <laylatdantas@gmail.com>
2019-05-24 04:32:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   contacts.each do |k, v|
      v.each do |k1,v1|
        if k1 ==:favorite_icecream_flavors
-        v1.delete_if?("strawberry")
+        v1.delete_if("strawberry")
         
          
        end
